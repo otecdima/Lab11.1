100e6ee3e2ac02c033d7ebbdd788487a10a62914
otecdima <93607175+otecdima@users.noreply.github.com>
2022-12-03 20:43:16 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = User.builder().name("Oles").age(31).occupation("UCU").gender(Gender.MALE).build();
+        System.out.println(user);
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,52 @@
+import lombok.Builder;
+import lombok.ToString;
+
+import java.util.List;
+
+enum Gender {
+    MALE, FEMALE
+}
+
+@Builder
+@ToString
+public class User {
+    private String name;
+    private int age;
+    private Gender gender;
+    private double weight;
+    private double height;
+    @Singular
+    public List<String> occupation;
+
+//    public User(String name, int age, Gender gender, double weight, double height) {
+//        this.name = name;
+//        this.gender = gender;
+//        this.age = age;
+//        this.weight = weight;
+//        this.height = height;
+//    }
+//
+//    public User(String name, int age, Gender gender) {
+//        this.name = name;
+//        this.gender = gender;
+//        this.age = age;
+//        this.weight = 0;
+//        this.height = 0;
+//    }
+//
+//    public User(String name, Gender gender) {
+//        this.name = name;
+//        this.gender = gender;
+//        this.age = 0;
+//        this.weight = 0;
+//        this.height = 0;
+//    }
+//
+//    public User(String name) {
+//        this.name = name;
+//        this.gender = null;
+//        this.age = 0;
+//        this.weight = 0;
+//        this.height = 0;
+//    }
+}
